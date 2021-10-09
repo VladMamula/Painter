6461ebd0ad8f2da269d79ab56aae403164e32ecf
marsel <vlad_mamula@mail.ru>
2021-10-09 15:48:21 +0300

    Start CartesianPainter

---
+++ app/src/main/java/com/example/painterapp/CartesianPainter.kt
@@ -0,0 +1,9 @@
+package com.example.painterapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CartesianPainter(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+
+}
