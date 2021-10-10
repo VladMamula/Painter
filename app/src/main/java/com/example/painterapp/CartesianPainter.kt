053228467a92c718e29c0fba008f85d74775f8a9
marsel <vlad_mamula@mail.ru>
2021-10-10 15:16:01 +0300

    Drawing Graph 2

--- app/src/main/java/com/example/painterapp/CartesianPainter.kt
+++ app/src/main/java/com/example/painterapp/CartesianPainter.kt
@@ -8,6 +8,7 @@ import android.util.AttributeSet
 import android.view.View
 import kotlin.math.roundToInt
 import kotlin.math.sin
+import kotlin.math.sqrt
 
 class CartesianPainter(context: Context, attrs: AttributeSet?) : View(context, attrs) {
     constructor(context: Context) : this(context, null)
@@ -33,7 +34,7 @@ class CartesianPainter(context: Context, attrs: AttributeSet?) : View(context, a
             drawAxes(this, converter)
             if (graph == 1)
                 drawGraph1(this, converter)
-            else drawGraph2()
+            else drawGraph2(this, converter)
 
         }
     }
@@ -47,7 +48,7 @@ class CartesianPainter(context: Context, attrs: AttributeSet?) : View(context, a
     }
 
     // Отрисовка первого графика
-    private fun drawGraph1(canvas: Canvas?, converter: CoordinatesConverter) {
+    private fun drawGraph1(canvas: Canvas, converter: CoordinatesConverter) {
         val points: MutableList<Pair<Double, Double>> = mutableListOf()
         var x = xMin
         val h = 0.1
@@ -56,12 +57,12 @@ class CartesianPainter(context: Context, attrs: AttributeSet?) : View(context, a
             points.add(Pair(x, y))
             x = ((x + h) * 10.0).roundToInt() / 10.0
         }
-        for (i in 0..((xMax - xMin) / 0.1 - 1).toInt()) {
+        for (i in 0 until points.size-1) {
             val p1X = converter.xCartToScreen(points[i].first)
             val p1Y = converter.yCartToScreen(points[i].second)
             val p2X = converter.xCartToScreen(points[i + 1].first)
             val p2Y = converter.yCartToScreen(points[i + 1].second)
-            canvas?.drawLine(
+            canvas.drawLine(
                 p1X.toFloat(),
                 p1Y.toFloat(),
                 p2X.toFloat(),
@@ -70,9 +71,37 @@ class CartesianPainter(context: Context, attrs: AttributeSet?) : View(context, a
             )
         }
     }
-    //Отрисовка второго графика
-    private fun drawGraph2() {
 
+    //Отрисовка второго графика
+    private fun drawGraph2(canvas: Canvas, converter: CoordinatesConverter) {
+        val points1: MutableList<Pair<Double, Double>> = mutableListOf()
+        val points2: MutableList<Pair<Double, Double>> = mutableListOf()
+
+        var x = xMin
+        val h = 0.1
+        while (x <= xMax) {
+            val y = sqrt(16 + 4 * x * x)
+            val y1 = -sqrt(16 + 4 * x * x)
+            points1.add(Pair(x, y))
+            points2.add(Pair(x, y1))
+            x = ((x + h) * 10.0).roundToInt() / 10.0
+        }
+
+        for (i in 0 until points1.size - 1) {
+            val p1X = converter.xCartToScreen(points1[i].first)
+            val p1Y = converter.yCartToScreen(points1[i].second)
+            val p2X = converter.xCartToScreen(points1[i + 1].first)
+            val p2Y = converter.yCartToScreen(points1[i + 1].second)
+            canvas.drawLine(p1X.toFloat(), p1Y.toFloat(), p2X.toFloat(), p2Y.toFloat(), graphPaint)
+        }
+
+        for (i in 0 until points2.size - 1) {
+            val p1X = converter.xCartToScreen(points2[i].first)
+            val p1Y = converter.yCartToScreen(points2[i].second)
+            val p2X = converter.xCartToScreen(points2[i + 1].first)
+            val p2Y = converter.yCartToScreen(points2[i + 1].second)
+            canvas.drawLine(p1X.toFloat(), p1Y.toFloat(), p2X.toFloat(), p2Y.toFloat(), graphPaint)
+        }
     }
 }
 
