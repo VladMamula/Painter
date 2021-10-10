cb67e836abae1e67dd4e7a539864127008fcd24a
marsel <vlad_mamula@mail.ru>
2021-10-10 15:07:21 +0300

    Drawing Graph 1

--- app/src/main/java/com/example/painterapp/CartesianPainter.kt
+++ app/src/main/java/com/example/painterapp/CartesianPainter.kt
@@ -6,6 +6,8 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.roundToInt
+import kotlin.math.sin
 
 class CartesianPainter(context: Context, attrs: AttributeSet?) : View(context, attrs) {
     constructor(context: Context) : this(context, null)
@@ -29,15 +31,48 @@ class CartesianPainter(context: Context, attrs: AttributeSet?) : View(context, a
 
             val converter = CoordinatesConverter(width, height, xMin, xMax, yMin, yMax)
             drawAxes(this, converter)
+            if (graph == 1)
+                drawGraph1(this, converter)
+            else drawGraph2()
+
         }
     }
 
+    // Отрисовка осей
     private fun drawAxes(canvas: Canvas?, converter: CoordinatesConverter) {
-
         val widthLine = converter.xCartToScreen(0.0)
         val heightLine = converter.yCartToScreen(0.0)
         canvas?.drawLine(widthLine.toFloat(), 0F, widthLine.toFloat(), height.toFloat(), axesPaint)
         canvas?.drawLine(0F, heightLine.toFloat(), width.toFloat(), heightLine.toFloat(), axesPaint)
+    }
+
+    // Отрисовка первого графика
+    private fun drawGraph1(canvas: Canvas?, converter: CoordinatesConverter) {
+        val points: MutableList<Pair<Double, Double>> = mutableListOf()
+        var x = xMin
+        val h = 0.1
+        while (x <= xMax) {
+            val y = 4 * x - 7 * sin(x)
+            points.add(Pair(x, y))
+            x = ((x + h) * 10.0).roundToInt() / 10.0
+        }
+        for (i in 0..((xMax - xMin) / 0.1 - 1).toInt()) {
+            val p1X = converter.xCartToScreen(points[i].first)
+            val p1Y = converter.yCartToScreen(points[i].second)
+            val p2X = converter.xCartToScreen(points[i + 1].first)
+            val p2Y = converter.yCartToScreen(points[i + 1].second)
+            canvas?.drawLine(
+                p1X.toFloat(),
+                p1Y.toFloat(),
+                p2X.toFloat(),
+                p2Y.toFloat(),
+                graphPaint
+            )
+        }
+    }
+    //Отрисовка второго графика
+    private fun drawGraph2() {
+
     }
 }
 
--- app/src/main/java/com/example/painterapp/MainActivity.kt
+++ app/src/main/java/com/example/painterapp/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Передача параметров в CartesianPainter
         initCartesianParameters()
 
         btn_confirm.setOnClickListener {
@@ -17,30 +18,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initCartesianParameters() {
+        //Цвет графика
         val graphColor = when (rg_graph_color.checkedRadioButtonId) {
             R.id.rb_graph_red -> 0xffff0000.toInt()
             R.id.rb_graph_blue -> 0xff0000ff.toInt()
             else -> 0xff000000.toInt()
         }
-
+        // Цвет осей
         val axesColor = when (rg_axes_color.checkedRadioButtonId) {
             R.id.rb_axes_red -> 0xffff0000.toInt()
             R.id.rb_axes_blue -> 0xff0000ff.toInt()
             else -> 0xff000000.toInt()
         }
-
+        //Толщина графика
         val graphWidth = when (rg_graph_width.checkedRadioButtonId) {
             R.id.rb_graph_thin -> 5F
             R.id.rb_graph_wide -> 15F
             else -> 10F
         }
-
+        // Номер графика
         val graph = when (rg_graph.checkedRadioButtonId) {
             R.id.rb_graph1 -> 1
             R.id.rb_graph2 -> 2
             else -> 1
         }
-
+        // Назначаем полученные параметры
         cartesian_painter.xMin = et_xMin.text.toString().toDouble()
         cartesian_painter.xMax = et_xMax.text.toString().toDouble()
         cartesian_painter.yMin = et_yMin.text.toString().toDouble()
@@ -49,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         cartesian_painter.graphPaint.strokeWidth = graphWidth
         cartesian_painter.axesPaint.color = axesColor
         cartesian_painter.graph = graph
+        // Отрисовываем
         cartesian_painter.invalidate()
     }
 }
